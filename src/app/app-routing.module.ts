ed3376cd851c38c6c085cc808976bc9ca8d901c4
u1810291 <o.rizakulov@student.inha.uz>
2021-04-14 12:47:45 +0500

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { BaseLayoutComponent } from './Layout/base-layout/base-layout.component';
 import { PagesLayoutComponent } from './Layout/pages-layout/pages-layout.component';
-
 // DEMO PAGES
-
 // Dashboards
-
 import { AnalyticsComponent } from './DemoPages/Dashboards/analytics/analytics.component';
-
 // Pages
-
 import { ForgotPasswordBoxedComponent } from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
 import { LoginBoxedComponent } from './DemoPages/UserPages/login-boxed/login-boxed.component';
 import { RegisterBoxedComponent } from './DemoPages/UserPages/register-boxed/register-boxed.component';
-
 // Elements
-
 import { StandardComponent } from './DemoPages/Elements/Buttons/standard/standard.component';
 import { DropdownsComponent } from './DemoPages/Elements/dropdowns/dropdowns.component';
 import { CardsComponent } from './DemoPages/Elements/cards/cards.component';
 import { ListGroupsComponent } from './DemoPages/Elements/list-groups/list-groups.component';
 import { TimelineComponent } from './DemoPages/Elements/timeline/timeline.component';
 import { IconsComponent } from './DemoPages/Elements/icons/icons.component';
-
 // Components
-
 import { AccordionsComponent } from './DemoPages/Components/accordions/accordions.component';
 // import {TabsComponent} from './DemoPages/Components/tabs/tabs.component';
 import { CarouselComponent } from './DemoPages/Components/carousel/carousel.component';
@@ -34,22 +24,14 @@ import { ModalsComponent } from './DemoPages/Components/modals/modals.component'
 import { ProgressBarComponent } from './DemoPages/Components/progress-bar/progress-bar.component';
 import { PaginationComponent } from './DemoPages/Components/pagination/pagination.component';
 import { TooltipsPopoversComponent } from './DemoPages/Components/tooltips-popovers/tooltips-popovers.component';
-
 // Tables
-
 import { TablesMainComponent } from './DemoPages/Tables/tables-main/tables-main.component';
-
 // Widgets
-
 import { ChartBoxes3Component } from './DemoPages/Widgets/chart-boxes3/chart-boxes3.component';
-
 // Forms Elements
-
 import { ControlsComponent } from './DemoPages/Forms/Elements/controls/controls.component';
 import { LayoutComponent } from './DemoPages/Forms/Elements/layout/layout.component';
-
 // Charts
-
 import { ChartjsComponent } from './DemoPages/Charts/chartjs/chartjs.component';
 
 const routes: Routes = [
